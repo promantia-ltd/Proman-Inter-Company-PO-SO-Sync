aee9b587768e60a6c6b37fa160496085a4794c2f
chethank1407 <chethank1407@gmail.com>
2025-03-10 16:16:40 +0530

    init: new app

---
+++ site_integration/site_integration/doctype/pispl_configuration/pispl_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Chethan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PISPL Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
