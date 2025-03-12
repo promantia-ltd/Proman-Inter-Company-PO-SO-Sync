71526955905eaee6c4dfb1f43d0e990207eddd94
chethank1407 <chethank1407@gmail.com>
2025-03-12 12:58:21 +0530

    refactor: code fixes

--- site_integration/api.py
+++ site_integration/api.py
@@ -1,18 +1,18 @@
 import frappe
 import requests
+from frappe.utils import formatdate
 
 @frappe.whitelist()
 def export_purchase_order_to_v15(po_name):
 	try:
 		doc = frappe.get_doc("Purchase Order", po_name)
 		config = frappe.get_single("PISPL Configuration")
+		password = config.get_password('password')
 		v15_url = config.url
 		headers = {
-			"Authorization": f"token {config.api_key}:{config.password}",
+			"Authorization": f"token {config.api_key}:{password}",
 			"Content-Type": "application/json"
 		}
-
-		customer = "Advanced Crushing Engineers Private Limited"
 
 		items = []
 		for item in doc.items:
@@ -21,27 +21,26 @@ def export_purchase_order_to_v15(po_name):
 				{"parent": item.item_code},
 				"supplier_part_no"
 			)
-			if not v15_item_code:
-				frappe.log_error(f"Item {item.item_code} has no supplier_part_no", "PO Export Error")
-				continue
 
 			items.append({
 				"item_code": v15_item_code,
 				"qty": item.qty,
-				"rate": item.rate
+				"rate": item.rate,
+				"delivery_date": item.schedule_date.strftime("%Y-%m-%d") if item.schedule_date else None
 			})
 
-		if not items:
-			frappe.log_error(f"PO {po_name} has no valid items to export", "PO Export Error")
-			return
-
 		payload = {
-			"customer": customer,
-			"items": items
+			"items": items,
+			"transaction_date": doc.transaction_date.strftime("%Y-%m-%d") if doc.transaction_date else None,
+			"delivery_date": doc.schedule_date.strftime("%Y-%m-%d") if doc.schedule_date else None
 		}
-		response = requests.post(f"{v15_url}/api/resource/Sales Order", json=payload, headers=headers)
+
+		frappe.log_error(title="payload", message=payload)
+
+		response = requests.post(f"{v15_url}/api/method/proman.proman.utils.create_sales_order.create_sales_order", json=payload, headers=headers)
 
 		if response.status_code == 200:
+			frappe.log_error(f"{response.text}", "PO Export Success")
 			return {"status": "success", "message": f"Sales Order created in PISPL v15 for PO {po_name}"}
 		else:
 			error_msg = f"Failed to export PO {po_name}: {response.text}"
